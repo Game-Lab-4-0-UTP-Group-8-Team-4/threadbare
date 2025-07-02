ccc5772352c6def6309257bfe71feb4a8ee45700
JoaquinPando <joaquin_a0603@hotmail.com>
2025-07-02 01:07:27 -0500

    add final narrative and add outro final

---
+++ scenes/quests/story_quests/neural-crisis/1_neural-crisis_stealth/neural_crisis_stealth.gd
@@ -0,0 +1,50 @@
+extends Node2D
+# VERSIÓN COMPLETA (recomendada)
+
+# Referencia al AudioStreamPlayer
+@onready var music_player = $Music/AudioStreamPlayer
+
+func _ready():
+	setup_background_music()
+	connect_audio_signals()
+
+func setup_background_music():
+	if music_player and music_player.stream:
+		music_player.autoplay = true
+		music_player.bus = "Master"
+		
+		# Configurar loop según el tipo de archivo
+		if music_player.stream is AudioStreamOggVorbis:
+			music_player.stream.loop = true
+		elif music_player.stream is AudioStreamMP3:
+			music_player.stream.loop = true
+		elif music_player.stream is AudioStreamWAV:
+			music_player.stream.loop_mode = AudioStreamWAV.LOOP_FORWARD
+		
+		if not music_player.playing:
+			music_player.play()
+
+func connect_audio_signals():
+	if music_player and not music_player.finished.is_connected(_on_music_finished):
+		music_player.finished.connect(_on_music_finished)
+
+func _on_music_finished():
+	music_player.play()
+
+func _process(_delta):
+	if music_player and music_player.stream and not music_player.playing:
+		music_player.play()
+
+# ==========================================
+# VERSIÓN SIMPLE (alternativa más básica)
+# ==========================================
+# extends Node
+# @onready var music_player = $music/AudioStreamPlayer
+# 
+# func _ready():
+#     if music_player:
+#         music_player.play()
+# 
+# func _process(_delta):
+#     if music_player and not music_player.playing:
+#         music_player.play()
